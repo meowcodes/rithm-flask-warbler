77d92d618aeb4297fc9d3f3b1b00b352e6c78d64
meowcodes <meowcodes@gmail.com>
2019-02-28 10:22:52 -0800

    update seed

--- seed.py
+++ seed.py
@@ -21,7 +21,10 @@ lena = User(
     username="meow",
     password="$2b$12$sPCpn3/c7WAd623GKiGNWesVRaZCdTsQlBGBILA9QYsJHbpCFA5lW",
     image_url="https://static.boredpanda.com/blog/wp-content/uploads/2016/08/Cute-kittens-46-57b323088a692__605.jpg",
-    email="meow@email.com")
+    email="meow@email.com",
+    id=304)
+
+db.session.add(FollowersFollowee(followee_id=72, follower_id=304))
 
 like1 = Like(user_id=100, msg_id=97)
 like2 = Like(user_id=72, msg_id=17)
