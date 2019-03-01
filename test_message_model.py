52da0e32f8628e5ca756e23f5d99cb2ffca3c540
Gabriela <balicas.gb@gmail.com>
2019-02-28 17:18:58 -0800

    add additional functionality tests

--- test_message_model.py
+++ test_message_model.py
@@ -1,6 +1,4 @@
 # test messages are being created
-# Too long = NAHHHHHHHH
-# Empty = HELL NAH
 # Test likes relaSHAWNSHAIP
 
 """Message model tests."""
@@ -65,10 +63,20 @@ class UserModelTestCase(TestCase):
     def test_message_model(self):
         """Does basic message model work?"""
 
+        m20 = Message(
+           id=1000020,
+           text=LOREM_IPSUM_REG,
+           user_id=1000
+        )
+        
+        db.session.add(m20)
+        db.session.commit()
         m1 = Message.query.get(1000001)
+        m10 = Message.query.get(1000020)
 
         self.assertEqual(m1.user_id, 1000)
         self.assertIsInstance(m1.timestamp, datetime)
+        self.assertIs(m20, m10)
 
 
     def test_repr(self):
@@ -134,74 +142,3 @@ class UserModelTestCase(TestCase):
         db.session.add(m3)
         self.assertRaises(IntegrityError, db.session.commit)
         db.session.rollback()
-
-    #     m1 = Message.query.get(1000001)
-
-    #     u2 = User(
-    #         id=2000,
-    #         email="test2@test.com",
-    #         username="testuser2",
-    #         password="HASHED_PASSWORD"
-    #     )
-
-    #     db.session.add(u2)
-    #     db.session.commit()
-
-    #     f = FollowersFollowee(followee_id=2000, follower_id=1000)
-        
-    #     db.session.add(f)
-    #     db.session.commit()
-
-    #     self.assertTrue(u1.is_followed_by(u2))
-    #     self.assertFalse(u2.is_followed_by(u1))
-
-
-    # def test_user_create(self):
-    #     """ Does User.create successfully create a new user given valid credentials and fail if validations fail? """
-    #     m1 = Message.query.get(1000001)
-    #     self.assertIs(User.query.get(1000), u1)
-
-    #     # same email
-    #     u2 = User(
-    #         id=2000,
-    #         email="test@test.com",
-    #         username="testuser2",
-    #         password="HASHED_PASSWORD"
-    #     )
-
-    #     # same username
-    #     u3 = User(
-    #         id=3000,
-    #         email="test2@test.com",
-    #         username="testuser",
-    #         password="HASHED_PASSWORD"
-    #     )
-
-    #     # no password
-    #     u4 = User(
-    #         id=4000,
-    #         email="test2@test.com",
-    #         username="testuser"
-    #     )
-
-    #     db.session.add(u2)
-    #     self.assertRaises(IntegrityError, db.session.commit)
-    #     db.session.rollback()
-
-    #     db.session.add(u3)
-    #     self.assertRaises(IntegrityError, db.session.commit)
-    #     db.session.rollback()
-
-    #     db.session.add(u4)
-    #     self.assertRaises(IntegrityError, db.session.commit)
-    #     db.session.rollback()
-
-
-    # def test_user_auth(self):
-    #     """ Does User.authenticate successfully return a user when given a valid username and password and fail if username/password are invalid? """
-
-    #     m1 = Message.query.get(1000001)
-
-    #     self.assertIs(User.authenticate(username="testuser", password="123456"), u1)
-    #     self.assertFalse(User.authenticate(username="testuser2", password="123456"))
-    #     self.assertFalse(User.authenticate(username="testuser", password="12345"))        
